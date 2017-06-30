ee679f640ed34292749344cb69c18d2269565677
Sharkstroke <fabio@MacBook-Pro-Retina-di-Fabio.local>
2017-06-30 16:22:50 +0200

    Notifica settata per essere inviata 10 minuti prima dell'inizio dell'attività

--- Agorun/app/src/main/java/com/unipd/fabio/agorun/MyNotificationManager.java
+++ Agorun/app/src/main/java/com/unipd/fabio/agorun/MyNotificationManager.java
@@ -10,7 +10,6 @@ import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
-import android.text.TextUtils;
 import android.util.Log;
 
 import java.text.SimpleDateFormat;
@@ -20,29 +19,26 @@ import java.util.Date;
 
 public class MyNotificationManager extends BroadcastReceiver {
 
+    private static final int TIMER = 10;
+
+    private String timeForNotification;
 
     @Override
     public void onReceive(Context context, Intent intent) {
         try {
-
-            //String yourDate = "30/06/2017";
-            //String yourHour = "23:08";
             String myFullHour = MySharedPreferencesHandler.getMySharedPreferencesString(context, MySharedPreferencesHandler.MyPreferencesKeys.joinedActivityHour, "");
             String myDate = MySharedPreferencesHandler.getMySharedPreferencesString(context, MySharedPreferencesHandler.MyPreferencesKeys.joinActivityDate, "");
 
             // Parso l'orario così da togliere i secondi e tenere soltando l'ora ed i minuti.
             String[] startParsed = myFullHour.split(":");
             String[] newHour = Arrays.copyOf(startParsed, startParsed.length - 1);
-            String myHour = TextUtils.join(":", newHour);
-            //String modifiedDate = new StringBuilder(myDate).reverse().toString();
-            //modifiedDate = modifiedDate.replaceAll("-","/");
 
-            System.out.println(myDate);
+            setNotificationTime(newHour);
 
             Date d = new Date();
             SimpleDateFormat date = new SimpleDateFormat("yyyy-MM-dd");
             SimpleDateFormat hour = new SimpleDateFormat("HH:mm");
-            if (myDate.equals(date.format(d)) && myHour.equals(hour.format(d))){
+            if (myDate.equals(date.format(d)) && timeForNotification.equals(hour.format(d))){
                 Intent it =  new Intent(context, MainActivity.class);
                 createNotification(context, it, "Agorun - Activity Reminder", "Agorun - Time to get ready!", "It's almost time! Get ready for your activity!");
             }
@@ -74,5 +70,22 @@ public class MyNotificationManager extends BroadcastReceiver {
             toque.play();
         }
         catch(Exception e){}
+    }
+
+    // Metodo che controlla e setta il tempo per la ricezione della notifica. Questa verrà mandata 10 minuti prima dell'inizio dell'attività, per cui si controllano i minuti per un eventuale cambio d'ora.
+    private void setNotificationTime(String[] newHour) {
+        int currentH = Integer.parseInt(newHour[0]);
+        int currentM = Integer.parseInt(newHour[1]);
+
+        if (currentM > 9) {
+            currentM -= TIMER;
+        } else {
+            int temp = 69;
+            temp -= TIMER;
+            currentM = temp;
+            currentH -= 1;
+        }
+
+        timeForNotification = currentH + ":" + currentM;
     }
 }
